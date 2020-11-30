a08e7f6a1e6eb8586caa67d35821d7cd3f5cc6b8
Daniel Estrada <isc.daniel.estrada@gmail.com>
2020-11-30 14:41:46 -0600

    ...

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask_socketio import SocketIO, send
 
 led = 14
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led, GPIO.OUT)
